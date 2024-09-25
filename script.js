0c56fbc8e9132039f4d2148865947aa5de47bc05
ttt229 <adnaneatmani22@gmail.com>
2024-09-25 04:46:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('experienceForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const experience = document.getElementById('experience').value;
+    alert('Thank you for sharing your experience!');});
