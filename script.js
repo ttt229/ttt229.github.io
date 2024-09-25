1786b1f864384e136de92560b0f59f46fe16c19b
ttt229 <adnaneatmani22@gmail.com>
2024-09-25 20:03:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+<><video id="responsiveVideo" controls>
+    Your browser does not support the video tag.
+</video><script>
+        const video = document.getElementById('responsiveVideo');
 
+        function loadVideo() { }
+        const screenWidth = window.innerWidth;
+        let videoSource = '';
+
+        if (screenWidth {'>'}= 1024) {videoSource = 'video-desktop.mp4'};
+        else if (screenWidth {'>'}= 768) {videoSource = 'video-tablet.mp4'};
+        else {videoSource = 'video-mobile.mp4'};
+        
+
+        video.src = videoSource;
+        video.load();
+    
+
+        // Load video on page load
+        window.onload = loadVideo;
+
+        // Reload video on window resize (optional)
+        window.onresize = loadVideo;
+    </script></>
 document.getElementById('experienceForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const experience = document.getElementById('experience').value;
